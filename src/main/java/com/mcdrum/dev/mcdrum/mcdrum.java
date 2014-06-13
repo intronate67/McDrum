03648ec12a9fa50e534b77df4740ddccce9150d2
Hunter Sharpe <huntersharpe@me.com>
2014-06-12 22:22:09 -0500

    Create mcdrum.java

---
+++ src/main/java/com/mcdrum/dev/mcdrum/mcdrum.java
@@ -0,0 +1,15 @@
+package com.mcdrum.dev.mcdrum;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class McDrum extends JavaPlugin{
+
+    public void onEnable(){
+
+    }
+    public void onDisable(){
+
+    }
+
+
+}
