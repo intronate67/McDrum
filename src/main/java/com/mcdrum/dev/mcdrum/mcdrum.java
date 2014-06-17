797d1844301b81ca0f090464f86533a94e36b2a1
Hunter Sharpe <huntersharpe@me.com>
2014-06-17 16:36:04 -0500

    Update mcdrum.java

--- src/main/java/com/mcdrum/dev/mcdrum/mcdrum.java
+++ src/main/java/com/mcdrum/dev/mcdrum/mcdrum.java
@@ -1,15 +1,27 @@
 package com.mcdrum.dev.mcdrum;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Created by Admin on 6/15/2014.
+ */
 public class McDrum extends JavaPlugin{
+    FileConfiguration config;
+    private McDrum instance = new McDrum();
 
+    @Override
     public void onEnable(){
+        if(!getDataFolder().exists()){
+            getDataFolder().mkdir();
+            config = getConfig();
+            saveDefaultConfig();
+        }
 
     }
+    @Override
     public void onDisable(){
 
     }
-
 
 }
